7d38cecaf99687751aab560cbb352d792d043b19
Andres Moron <andresmoron622@gmail.com>
2019-02-23 10:23:28 -0400

    Se creo la funcion suma

---
+++ wsAritmetica/WCFaritmetica/IsrvSuma.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFaritmetica
+{
+    [ServiceContract]
+    public interface IsrvSuma
+    {
+
+        [OperationContract]
+        clsSuma suma(int nummero1, int numero2);
+    }
+
+    [DataContract]
+    public class clsSuma
+    {
+        [DataMember]
+        public int numeroA { get; set; }
+
+        [DataMember]
+        public int numeroB { get; set; }
+
+        [DataMember]
+        public int Resultado { get; set; }
+
+
+    }
+}
